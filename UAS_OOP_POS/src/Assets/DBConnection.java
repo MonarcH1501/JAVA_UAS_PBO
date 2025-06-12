fee397dc46180641855241473f4e9f3faac723d0
MonarcH1501 <stevenedmund18@gmail.com>
2025-06-12 07:22:37 +0700

    perbaiki pada bagian dbconnection

--- UAS_OOP_POS/src/Assets/DBConnection.java
+++ UAS_OOP_POS/src/Assets/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DBConnection {
    public static Connection getConnection() throws SQLException{
-       String url = "jdbc:mysql://localhost:3306/uas_oop2";
+       String url = "jdbc:mysql://localhost:3306/uas_oop";
        String user = "root";
        String password = "";
        
